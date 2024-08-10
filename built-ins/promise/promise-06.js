a0bc22880eaf6feef35aa84f812c21444c37da3c
feng.zhang <zfeng0712@gmail.com>
2024-08-10 17:12:52 +0800

    feat: add empty file for manual bind

---
+++ built-ins/promise/promise-06.js
@@ -0,0 +1,28 @@
+// 下面的代码执行结果是什么？
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/await
+
+let i = 0;
+
+queueMicrotask(function test() {
+  i++;
+  console.log("microtask", i);
+  if (i < 3) {
+    queueMicrotask(test);
+  }
+});
+
+(async () => {
+  console.log("async function start");
+  for (let i = 1; i < 3; i++) {
+    await null;
+    console.log("async function resume", i);
+  }
+  await null;
+  console.log("async function end");
+})();
+
+queueMicrotask(() => {
+  console.log("queueMicrotask() after calling async function");
+});
+
+console.log("script sync part end");
